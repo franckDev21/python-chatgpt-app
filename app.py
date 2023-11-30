ab77fac2f591e8b321afcb82e5cf9d5c12203bdd
franckDev21 <tutodevphp@gmail.com>
2023-11-30 14:04:33 +0100

    update app.py

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
 import os
-import openai
+from openai import OpenAI
+
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask
+from flask import Response
+from flask import render_template
+from flask import request
+
+load_dotenv()
 
 app = Flask(__name__)
 
+client = OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/prompt', methods=['POST'])
+def prompt():
+    messages = request.json['messages']
+    conversation = build_conversation_dict(messages=messages)
 
-if __name__ == '__main__':
+    return Response(event_stream(conversation), mimetype='text/event-stream')
+
+def event_stream(conversation: list[dict]) -> str:
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=conversation,
+        stream=True,
+    )
+    
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            yield chunk.choices[0].delta.content
+
+def build_conversation_dict(messages: list) -> list[dict]:
+    return [
+        {"role": "user" if i % 2 == 0 else "assistant", "content": message}
+        for i, message in enumerate(messages)
+    ]
+
+
+if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=5000)
